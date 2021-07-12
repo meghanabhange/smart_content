6044b79c832ee2db65ac64e499ce4815fd9993de
meghanabhange <meghanabhange13@gmail.com>
2021-07-13 00:14:24 +0530

    Update

--- smart/extract.py
+++ smart/extract.py
@@ -2,12 +2,35 @@ import json
 from pathlib import Path
 
 import fire
+import pandas as pd
+import requests
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 
 from smart.settings import DATAPATH, HTMLPATH
 
 test_file = HTMLPATH / "03-conditional.html"
+
+df = pd.read_csv("map_concepts_activities_topics_Python_MasteryGrids_latest_course.csv")
+
+
+def get_parsed_text(code):
+    data = {"code": code, "mode": "simple"}
+    response = requests.post("http://acos.cs.hut.fi/python-parser", data=data)
+    try :
+        output = [v for k, v in response.json()["lines"].items()][0]
+        return output
+    except:
+        return []
+
+
+def get_smart_content(code_types):
+    output = []
+    for t in code_types:
+        matched_content = df[df["component_name"] == t]
+        for i, row in matched_content.iterrows():
+            output.append({"url": row["url"], "topic_name": row["topic_name"]})
+    return {"Matched Smart Content": output}
 
 
 def next_element(elem):
@@ -45,10 +68,31 @@ def extract(output_file="data/output.json"):
         output[file_name] = {"title": title, "content": []}
 
         for page in pages:
-            code = [ele.getText() for ele in page.find_all("pre", {"class": "python"})]
+            all_code = [ele.getText() for ele in page.find_all("pre", {"class": "python"})]
             text = [ele.getText() for ele in page.find_all("p")]
+            code_segment = []
+            if all_code:
+                for code in all_code:
+                    codelines = code.split("\n")
+                    execcode = [line[4:] for line in codelines if line[:3] == ">>>"]
+                    outputcode = [line for line in codelines if line[:3] != ">>>"]
+                    execcode = "\n".join(execcode)
+                    outputcode = "\n".join(outputcode)
+                    if execcode:
+                        code_types = get_parsed_text(execcode)
+                        matched_content = get_smart_content(code_types)
+                        code_segment.append({
+                            "code" : code,
+                            "execcode": execcode,
+                            "matched_content": matched_content,
+                        })
+
             output[file_name]["content"].append(
-                {"Topic": page.find("h2").getText(), "code": code, "text": text}
+                {
+                    "Topic": page.find("h2").getText(),
+                    "code_segment": code_segment,
+                    "text": text
+                }
             )
     output_file = Path(output_file)
     json.dump(output, output_file.open("w"), indent=2)
