f9b4811e7be2415ed3fa0ca6a1227b45dbc32e96
meghanabhange <meghanabhange13@gmail.com>
2021-07-26 02:31:57 +0530

    Add new data

--- smart/extract.py
+++ smart/extract.py
@@ -5,12 +5,10 @@ import fire
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+from fuzzywuzzy import fuzz, process
 from tqdm import tqdm
 
 from smart.settings import DATAPATH, HTMLPATH
-
-from fuzzywuzzy import fuzz
-from fuzzywuzzy import process
 
 test_file = HTMLPATH / "03-conditional.html"
 
@@ -20,11 +18,12 @@ df = pd.read_csv("map_concepts_activities_topics_Python_MasteryGrids_latest_cour
 def get_parsed_text(code):
     data = {"code": code, "mode": "simple"}
     response = requests.post("http://acos.cs.hut.fi/python-parser", data=data)
-    try :
+    try:
         output = [v for k, v in response.json()["lines"].items()][0]
         return output
     except:
         return []
+
 
 def get_smart_content(code_types, keywords):
     output = []
@@ -32,7 +31,7 @@ def get_smart_content(code_types, keywords):
         matched_content = df[df["component_name"] == t]
         for i, row in matched_content.iterrows():
             extacted = process.extractOne(row["topic_name"], keywords)
-            if extacted and extacted[1]>70:
+            if extacted and extacted[1] > 70:
                 output.append({"url": row["url"], "topic_name": row["topic_name"]})
     return {"Matched Smart Content": output[:10]}
 
@@ -74,28 +73,33 @@ def extract(output_file="data/output.json"):
         all_topics = [ele.getText() for ele in soup.find_all("h2")]
 
         for page in pages:
-            all_code = [ele.getText() for ele in page.find_all("pre", {"class": "python"})]
+            all_code = [
+                ele.getText() for ele in page.find_all("pre", {"class": "python"})
+            ]
             text = [ele.getText() for ele in page.find_all("p")]
-            
             code_segment = []
             if all_code:
                 for code in all_code:
                     codelines = code.split("\n")
                     execcode = codelines
                     outputcode = []
-                    if not(code.find(">>>")==-1):
+                    if not (code.find(">>>") == -1):
                         execcode = [line[4:] for line in codelines if line[:3] == ">>>"]
                         outputcode = [line for line in codelines if line[:3] != ">>>"]
                     execcode = "\n".join(execcode)
                     outputcode = "\n".join(outputcode)
                     if execcode:
-                        keywords = [concept for concept in concepts if concept in " ".join(text).split()]
-                        code_types = get_parsed_text(execcode)
-                        matched_content = get_smart_content(code_types, keywords)
-                        code_segment.append({
-                            "execcode": execcode,
-                            "matched_content": matched_content,
-                        })
+                        keywords = [
+                            concept
+                            for concept in concepts
+                            if concept in " ".join(text).split()
+                        ]
+                        code_segment.append(
+                            {
+                                "execcode": execcode,
+                                "outputcode": outputcode,
+                            }
+                        )
             topic = page.find("h2").getText()
             if not (topic in ("Glossary", "Exercises")):
                 output[file_name]["content"].append(
@@ -103,7 +107,7 @@ def extract(output_file="data/output.json"):
                         "Topic": topic,
                         "code_segment": code_segment,
                         "text": text,
-                        "code" : all_code
+                        # "code": all_code,
                     }
                 )
         output[file_name]["all_topics"] = all_topics
